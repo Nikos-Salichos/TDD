e94491c5e43b31b72e67dc18b2aa08d4ec53c398
Nikos S <niksal1991@gmail.com>
2022-12-10 14:14:43 +0200

    Converted IRepository to generic

---
+++ Infrastructure/Repository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Domain
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected DbContext DbContext { get; private set; }
+
+        public Repository(DbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+
+    }
+}
