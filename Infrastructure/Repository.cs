34d4f28aa895164d7e06146a8bf244b7eea8d54d
Nikos S <niksal1991@gmail.com>
2022-12-10 14:17:05 +0200

    Added Repository Get method

--- Infrastructure/Repository.cs
+++ Infrastructure/Repository.cs
@@ -1,16 +1,66 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace Domain
 {
     public class Repository<T> : IRepository<T> where T : class
     {
-        protected DbContext DbContext { get; private set; }
+        private readonly DbContext _dbContext;
 
         public Repository(DbContext dbContext)
         {
-            DbContext = dbContext;
+            _dbContext = dbContext;
         }
 
+        public async Task<T> Get(string id)
+        {
+            var entity = await _dbContext.Set<T>().FindAsync(id);
+            return entity;
+        }
 
+        public Task<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetWhere(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
