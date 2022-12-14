e44fc6a074afd436a73153f28dab30abda5c9cd8
Nikos S <niksal1991@gmail.com>
2022-12-14 19:05:51 +0200

    Added RoomBookingRequest.cs

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+namespace Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Should_Return_Room_Booking_Request()
+        {
+            //Arrange
+            var bookingRequest = new RoomBookingRequest
+            {
+
+            }
+
+            //Act
+
+            //Assert
+        }
+    }
+}
