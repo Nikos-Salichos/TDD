aae69e1bc99288cf70cb1c8ff106803d4d271695
Nikos S <niksal1991@gmail.com>
2022-12-15 18:34:54 +0200

    Added reference

--- Tests/Models/RoomBookingRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tests.Models
-{
-    public class RoomBookingRequest
-    {
-        public string FullName { get; set; }
-        public string Email { get; set; }
-        public DateTime Date { get; set; }
-    }
-}
